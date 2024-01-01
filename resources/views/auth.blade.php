157c0aeb8237c96511303e23ef2ee4dc01bdda7f
Zen Dejavu <arsanptik05@gmail.com>
2024-01-01 20:40:50 +0800

    Add js_auth at line 122 in auth blade

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -91,35 +91,35 @@
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('/') }}assets/img/logo.ico" sizes="any">
         
-            <!-- Demo script -->
-            <script>
-                var themeConfig = {
-                    theme: JSON.parse('"light"'),
-                    isRTL: JSON.parse('false'),
-                    isFluid: JSON.parse('true'),
-                    sidebarBehaviour: JSON.parse('"fixed"'),
-                    navigationColor: JSON.parse('"inverted"')
-                };
-                
-                var isRTL = localStorage.getItem('isRTL') === 'true',
-                    isFluid = localStorage.getItem('isFluid') === 'true',
-                    theme = localStorage.getItem('theme'),
-                    sidebarSizing = localStorage.getItem('sidebarSizing'),
-                    linkLTR = document.getElementById('stylesheetLTR'),
-                    linkRTL = document.getElementById('stylesheetRTL'),
-                    html = document.documentElement;
-        
-                if (isRTL) {
-                    linkLTR.setAttribute('disabled', '');
-                    linkRTL.removeAttribute('disabled');
-                    html.setAttribute('dir', 'rtl');
-                } else {
-                    linkRTL.setAttribute('disabled', '');
-                    linkLTR.removeAttribute('disabled');
-                    html.removeAttribute('dir');
-                }
-            </script>
-        
+        <!-- Demo script -->
+        <script>
+            var themeConfig = {
+                theme: JSON.parse('"light"'),
+                isRTL: JSON.parse('false'),
+                isFluid: JSON.parse('true'),
+                sidebarBehaviour: JSON.parse('"fixed"'),
+                navigationColor: JSON.parse('"inverted"')
+            };
+            
+            var isRTL = localStorage.getItem('isRTL') === 'true',
+                isFluid = localStorage.getItem('isFluid') === 'true',
+                theme = localStorage.getItem('theme'),
+                sidebarSizing = localStorage.getItem('sidebarSizing'),
+                linkLTR = document.getElementById('stylesheetLTR'),
+                linkRTL = document.getElementById('stylesheetRTL'),
+                html = document.documentElement;
+    
+            if (isRTL) {
+                linkLTR.setAttribute('disabled', '');
+                linkRTL.removeAttribute('disabled');
+                html.setAttribute('dir', 'rtl');
+            } else {
+                linkRTL.setAttribute('disabled', '');
+                linkLTR.removeAttribute('disabled');
+                html.removeAttribute('dir');
+            }
+        </script>
+        @yield('js_auth')
         <!-- Page Title -->
         <title>@yield('title') | BikinDingin</title>
     </head>
