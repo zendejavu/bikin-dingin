5a6453b344d5c0e9d6f7b1a60bb7cb1138220325
Zen Dejavu <arsanptik05@gmail.com>
2024-01-01 20:39:23 +0800

    Add store and authenticate method in Auth Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller
 {
@@ -19,5 +24,39 @@ class AuthController extends Controller
     public function reset_pass()
     {
         return view('auth.reset');
+    }
+    
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users',
+            'email' => 'required|min:3|max:255|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        $validated['password'] = bcrypt($validated['password']);
+
+        User::create($validated);
+        
+        return redirect('/login')->with('success', 'Akun Anda telah terdaftar');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email:dns'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->with([
+            'email' => 'Email Anda belum terdaftar',
+        ])->onlyInput('email');
     }
 }
