213cb8b0c2892f016156a7e1144bf062c7e2015c
Zen Dejavu <arsanptik05@gmail.com>
2024-01-06 11:17:54 +0800

    Add data User to admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $users = User::all();
+        return view('admin.index', ['users' => $users]);
     }
 }
